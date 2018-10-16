c7ffadeb03742e2faffb2503dcd7814f29e54084
Joseph <josephjeshurun26@gmail.com>
2018-10-16 11:46:06 +0530

    Hide the text being typed in Password_Box and Edit the layout of widgets

--- login_gui.py
+++ login_gui.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLineEdit, QGridLayout
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 from getpass import getpass
  
@@ -28,26 +28,31 @@ class LoginWindow(QWidget):
         else :
             event.ignore()
 
-        ###### Quit Button
-    def createQuitButton(self) :
-        self.quit_button = QPushButton('Quit', self)
-        self.quit_button.clicked.connect(self.close)
-        self.quit_button.resize(self.quit_button.sizeHint())
-        self.quit_button.move(100,200)
-        return self.quit_button
+        ###### Username Label
+    def usernameLabel(self) :
+    	self.unameLabel = QLabel(self)
+    	self.unameLabel.setText("Username")
+    	self.unameLabel.move(20, 0)
 
         ###### Inputting Username
     def enterUsername(self) :
-        self.uname_box = QLineEdit('Username', self)
+        self.uname_box = QLineEdit(self)
         self.uname_box.move(20, 20)
         self.uname_box.resize(280,40)
         username = self.uname_box.text()
         return self.uname_box
 
+        ###### Password Label
+    def passwordLabel(self) :
+    	self.pwdLabel = QLabel(self)
+    	self.pwdLabel.setText("Password")
+    	self.pwdLabel.move(20, 80)
+
         ###### Inputting Password
     def enterPassword(self) :
-        self.pwd_box = QLineEdit('Password', self)
-        self.pwd_box.move(20, 80)
+        self.pwd_box = QLineEdit(self)
+        self.pwd_box.setEchoMode(QLineEdit.Password)
+        self.pwd_box.move(20, 100)
         self.pwd_box.resize(280,40)
         password = self.pwd_box.text()
         return self.pwd_box
@@ -55,19 +60,43 @@ class LoginWindow(QWidget):
         ###### Login Button
     def createLoginButton(self) :
         self.login_button = QPushButton('Login',self)
-        self.login_button.move(20, 120)
+        #self.login_button.move(20, 140)
         self.login_button.clicked.connect(self.on_login)
         return self.login_button
+
+        ###### Quit Button
+    def createQuitButton(self) :
+        self.quit_button = QPushButton('Quit', self)
+        self.quit_button.clicked.connect(self.close)
+        self.quit_button.resize(self.quit_button.sizeHint())
+        #self.quit_button.move(160,140)
+        return self.quit_button
+
+        ###### Layout for Login and Quit Buttons
+    def createButtonLayout(self):
+    	self.horizontalGroupBox = QGroupBox()
+    	self.layout = QHBoxLayout()
+    	self.login_button = self.createLoginButton()
+    	self.layout.addWidget(self.login_button)
+    	self.quit_button = self.createQuitButton()
+    	self.layout.addWidget(self.quit_button)
+    	self.horizontalGroupBox.setLayout(self.layout)
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        quit_button = self.createQuitButton()
-        login_button = self.createLoginButton()
+        self.createButtonLayout()
+        #quit_button = self.createQuitButton()
+        #login_button = self.createLoginButton()
+
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
+
         uname_box = self.enterUsername()
         pwd_box = self.enterPassword()
-
-        #self.statusBar().showMessage('Message in statusbar.')
+        self.usernameLabel()
+        self.passwordLabel()
         self.show()
     
     '''
@@ -82,6 +111,7 @@ class LoginWindow(QWidget):
     def on_login(self) :
         print('Username is '+ self.uname_box.text())
         self.close()
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = LoginWindow()
